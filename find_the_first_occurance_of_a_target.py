537d834100e344d93bdea42925fdd0c2d1de9aaf
Yousef <yousefm2315@gmail.com>
2025-01-05 01:38:23 -0500

    eeeeeeeeeeeeeee

---
+++ find_the_first_occurance_of_a_target.py
@@ -0,0 +1,25 @@
+def findFirstOccurrence(nums: List[int], target: int) -> int:
+    l , r = 0 , len(nums) - 1
+    
+    
+    while l <= r:
+        cur = (l + r) // 2
+        
+        if target > nums[cur]:
+            l = cur + 1
+        elif target < nums[cur]:
+            r = cur - 1
+        elif cur > 0 and nums[cur] == nums[cur - 1]:
+            r = cur - 1 
+        else:
+            return cur
+    return -1
+        
+        
+        
+        
+        
+        
+        
+        
+        
