d59322ef1407f9772de18c02dd25a6a92844fe98
Yousef <yousefm2315@gmail.com>
2025-01-05 23:22:29 -0800

    group anagrams

---
+++ 49_group_anagrams.py
@@ -0,0 +1,17 @@
+def groupAnagrams(strs):
+    
+    hashmap = {}
+    
+    for x in strs:
+        arrKey = []
+        
+        for c in x:
+            cur = ord(c) - ord("a")
+            arrKey.append(cur)
+            
+        if arrKey not in hashmap:
+            hashmap[tuple(arrKey)] = [x]
+        else:
+            hashmap[tuple(arrKey)].append(x)
+        
+    return list(hashmap.values())
