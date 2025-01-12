9614ccf8058e5f83c214afb892d60525afd312e1
Yousef <yousefm2315@gmail.com>
2025-01-12 02:54:19 -0500

    fix

--- 268_missing_number.py
+++ 268_missing_number.py
@@ -5,10 +5,10 @@ def missingNumber(nums: List[int]) -> int:
     nums.sort()
     
     
-    for x in range(len(nums)):
+    for x in range(1, len(nums)):
 
-        if x == 0:
-            continue
+        if nums[0] != 0:
+            return 0
 
         diff = nums[x] - nums[x - 1]
 
