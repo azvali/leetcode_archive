d065994a64a9cd62e75e49998c8fd54c3cb43a67
Yousef <yousefm2315@gmail.com>
2025-01-13 03:28:27 -0500

    ee

--- 2_128_longest_consecutive_sequence.py
+++ 2_128_longest_consecutive_sequence.py
@@ -1,2 +1,22 @@
 def longest_consecutive(nums: list[int]) -> int:
-    pass
+    
+    hashset = set(nums)
+    res = 0
+    cur = 0
+    
+    for x in hashset:
+        
+        if x - 1 not in hashset:
+            cur = 1
+
+            while x + cur in hashset:
+                cur += 1
+            
+            res = max(res, cur)
+                    
+    return res
+
+        
+        
+            
+        
