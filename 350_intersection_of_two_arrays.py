83d6cca86e1a8370c2c97ab87d9916f2e8ede8fa
Yousef <yousefm2315@gmail.com>
2025-01-15 04:30:15 -0500

    ez

--- 350_intersection_of_two_arrays.py
+++ 350_intersection_of_two_arrays.py
@@ -9,9 +9,7 @@ def intersect(nums1: list[int], nums2: list[int]) -> list[int]:
             
     for x in nums1:
         
-        if x in hashmap and hashmap[x] > 0:
-            freq = hashmap.get(x, 0)
-            
+        if x in hashmap and hashmap[x] > 0:   
             res.append(x)
             hashmap[x] = hashmap.get(x, 0) - 1
             
