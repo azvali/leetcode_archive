d21aafc45f478dcf05beead29b53095a9fa149e4
Yousef <yousefm2315@gmail.com>
2025-01-18 11:34:41 -0500

    e

---
+++ 238_move_zeros.py
@@ -0,0 +1,19 @@
+def moveZeroes(nums):
+    
+    l, r  = 0, 0
+    
+    while r < len(nums):
+           
+        if nums[r] != 0:
+            nums[l] , nums[r] = nums[r], nums[l]
+            l += 1
+        
+        r += 1
+            
+            
+    return nums
+            
+            
+        
+        
+       
