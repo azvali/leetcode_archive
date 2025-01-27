b9b5a704f02a0e85c5d33ae802a7c12aa54bfce9
yousef <yousefm2315@gmail.com>
2025-01-27 01:02:02 -0500

    got rid of l and r

--- 986_interval_list_intersection.py
+++ 986_interval_list_intersection.py
@@ -4,16 +4,14 @@ def intervalIntersection(firstList: list[list[int]], secondList: list[list[int]]
     if not firstList or not secondList:
         return []
     
-    l1, r1 = 0, 1
-    l2, r2 = 0, 1
     res = []
 
     i , j = 0, 0
     
     while i < len(firstList) and j < len(secondList):
         
-        start1, finish1 = firstList[i][l1], firstList[i][r1]
-        start2, finish2 = secondList[j][l2], secondList[j][r2]
+        start1, finish1 = firstList[i][0], firstList[i][1]
+        start2, finish2 = secondList[j][0], secondList[j][1]
         
         if start1 <= finish2 and start2 <= finish1:
             res.append([max(start1, start2), min(finish1, finish2)])
