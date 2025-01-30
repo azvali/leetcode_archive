23cd32c0e24be4fc7e34b7c271a33ce09cb81385
yousef <yousefm2315@gmail.com>
2025-01-29 23:40:54 -0500

    antiamaxwin

---
+++ 643_max_avg_subarray1.py
@@ -0,0 +1,23 @@
+def findMaxAverage(nums: List[int], k: int) -> float:
+
+
+    l,r  = 0, 0
+    res = float('-inf')
+    cur = 0
+    
+    while r < k:
+        cur += nums[r]
+        r += 1
+    
+    avg = cur / k 
+    res = max(res, avg)
+        
+        
+    while r < len(nums):
+        cur += nums[r]
+        cur -= nums[l] 
+        res = max(res, cur / k )
+        r += 1
+        l += 1
+        
+    return res
