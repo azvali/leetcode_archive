fd339972748142df83c788e07aaee3750cd0d71d
Yousef <yousefm2315@gmail.com>
2025-02-19 20:40:27 -0500

    e

--- 162_find_peak_elemen.py
+++ 162_find_peak_elemen.py
@@ -1,4 +1,14 @@
 def findPeakElement(nums: list[int]) -> int:
     
     
+    l , r = 0 , len(nums) - 1
     
+    while l < r:
+        cur = l + (r - l) // 2
+        
+        if nums[cur] > nums[cur + 1]:
+            r = cur
+        else:
+            l = cur + 1
+            
+    return l
