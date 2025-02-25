12ed7c6792c7cf87b47aa2c9cc3e644118e478b6
Yousef Mardrosyan <104654454+azvali@users.noreply.github.com>
2025-02-25 04:00:04 -0500

    e

--- 852_peak_index_in_mountain_arr.py
+++ 852_peak_index_in_mountain_arr.py
@@ -1,4 +1,16 @@
 def peakIndexInMountainArray(arr: list[int]) -> int:
     
     
+    l , r = 0 , len(arr) - 1
     
+    while l < r:
+        mid = l + (r - l) // 2
+        
+        
+        if arr[mid] < arr[mid + 1]:
+            l = mid + 1
+        else:
+            r = mid
+            
+    return l
+        
