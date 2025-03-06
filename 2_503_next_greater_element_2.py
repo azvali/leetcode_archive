d6c821930b984b1b4c57d9efec887f6004f54156
Yousef <yousefm2315@gmail.com>
2025-03-06 01:57:19 -0500

    e

---
+++ 2_503_next_greater_element_2.py
@@ -0,0 +1,16 @@
+def nextGreaterElements(self, nums: List[int]) -> List[int]:
+    
+    n = len(nums)
+    res = [-1] * len(nums)
+    stack = []
+    
+    for i in range(2 * n):
+        while stack and nums[i % n] > nums[stack[-1]]:
+            index = stack.pop()
+            res[index] = nums[i % n]
+        
+        if i < n:
+            stack.append(i)
+        
+        
+    return res
