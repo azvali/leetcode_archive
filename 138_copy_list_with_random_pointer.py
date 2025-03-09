5092671ac923be18a6b8eebcac49df9bfcb9cef6
Yousef <yousefm2315@gmail.com>
2025-03-09 07:31:51 -0400

    e

---
+++ 138_copy_list_with_random_pointer.py
@@ -0,0 +1,18 @@
+def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+    
+    hashmap = {None: None}
+    curr = head
+
+    while curr:
+        copy = Node(curr.val)
+        hashmap[curr] = copy
+        curr = curr.next
+    
+    curr = head
+    while curr:
+        copy = hashmap[curr]
+        copy.next = hashmap[curr.next]
+        copy.random  = hashmap[curr.random]
+        curr = curr.next
+
+    return hashmap[head]
