a857986a6612684a67f568f416490e13ae78f548
Yousef <yousefm2315@gmail.com>
2025-03-12 04:57:48 -0400

    e

--- 2_19_remove_nth_node_from_end.py
+++ 2_19_remove_nth_node_from_end.py
@@ -1,2 +1,25 @@
 def removeNthFromEnd(head: ListNode, n: int) -> ListNode:
-    pass  # Implement logic here
+    
+    
+    dummy = ListNode(0)
+    dummy.next = head
+    l , r = dummy, dummy
+
+    
+    for _ in range(n + 1):
+        if r:
+            r = r.next
+        
+    while r :
+        l = l.next
+        r = r.next
+    
+    if l.next:
+        l.next = l.next.next
+    
+    
+    return dummy.next
+    
+    
+        
+        
