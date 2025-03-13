d71e248940056fa9e23505c20c61a7f7b6d64147
Yousef <yousefm2315@gmail.com>
2025-03-13 06:34:34 -0400

    e

---
+++ 142_linked_list_cycle_2.py
@@ -0,0 +1,18 @@
+def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    
+    slow , fast = head , head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+        if slow == fast:
+            slow2 = head
+
+            while slow2 != slow:
+                slow2 = slow2.next
+                slow = slow.next
+
+            return slow2
+
+    return None
