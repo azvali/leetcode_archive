7cc697fc5ff01e8ce306bb4a7872046629bdf80c
Yousef <yousefm2315@gmail.com>
2025-03-27 04:52:36 -0400

    e

---
+++ 129_sum_room_to_leaf_node.py
@@ -0,0 +1,21 @@
+def sumNumbers(self, root: Optional[TreeNode]) -> int:
+    
+    if not root:
+        return 0
+
+
+    def dfs(node, num):
+
+        if not node:
+            return 0
+            
+        num = num * 10 + node.val
+
+        if not node.left and not node.right:
+            return num
+
+            
+        return dfs(node.left, num) + dfs(node.right, num)
+
+    return dfs(root, 0)
+    
