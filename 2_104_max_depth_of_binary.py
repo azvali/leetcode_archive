9609cfa66dbd562d77ffa260a7a1a47f712d7f82
Yousef <yousefm2315@gmail.com>
2025-03-31 05:34:25 -0400

    e

--- 2_104_max_depth_of_binary.py
+++ 2_104_max_depth_of_binary.py
@@ -1,6 +1,6 @@
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        
-        if not root:
-            return 0
+def maxDepth(self, root: Optional[TreeNode]) -> int:
+    
+    if not root:
+        return 0
 
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
