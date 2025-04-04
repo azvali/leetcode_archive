de5e541aa0ba05ae3c58b94a5e1ce00d4b8a8e99
Yousef <yousefm2315@gmail.com>
2025-04-03 21:53:49 -0400

    e

---
+++ 2_230_kth_smallest.py
@@ -0,0 +1,16 @@
+def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+            
+    res = 0
+    stack = []
+    curr = root
+
+    while curr or stack:
+        while curr:
+            stack.append(curr)
+            curr = curr.left
+
+        curr = stack.pop()
+        res += 1
+        if res == k:
+            return curr.val
+        curr = curr.right
